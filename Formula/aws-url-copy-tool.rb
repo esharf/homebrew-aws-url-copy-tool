06b7e8329e7066bec20aa140fb0d5beac94012fc
esharf <runner@Mac-1759203734063.local>
2025-09-30 04:30:37 +0000

    Update formula file to version 1.0.1

--- Formula/aws-url-copy-tool.rb
+++ Formula/aws-url-copy-tool.rb
@@ -1,9 +1,9 @@
 class AwsUrlCopyTool < Formula
   desc "Auto-modify macOS clipboard for AWS URLs"
   homepage "https://github.com/esharf/aws-url-copy-tool"
-  url "https://github.com/esharf/aws-url-copy-tool/releases/download/v1.0.0/aws-url-copy-tool-macos.zip"
-  sha256 "ae09e5a089abe9fc30f6b4c658ae54904ce63f59200b6a209e294f020f1e2737"
-  version "1.0.0"
+  url "https://github.com/esharf/aws-url-copy-tool/releases/download/v1.0.1/aws-url-copy-tool-macos.zip"
+  sha256 "2fd8d7362cdb1e3a08663862719f2a82d466fe8267343eba208fd185fcf7d673"
+  version "1.0.1"
 
   def install
     bin.install "aws-url-copy-tool"
